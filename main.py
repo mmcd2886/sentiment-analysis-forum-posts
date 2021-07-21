64b96c0946d2a14857ca422986e02e90ae24923c
mmcdonald2886 <1>
2021-07-21 18:08:30 -0400

    added if statements to only get the thread information one time, so it is not scraped for every page. Added if statement so will only get the last page of thread once so it is not scraped for every page

--- main.py
+++ main.py
@@ -45,9 +45,11 @@ last_scraped_page_query = connection.execute("SELECT last_page_scraped FROM poll
 try:
     forum_thread_page_num = int(last_scraped_page_query.fetchone()[0])
     print("The furthest page scraped is: ", forum_thread_page_num)
+    url_has_already_been_scraped = "Yes"
 except TypeError:
-    print("This url has not been scraped yet")
+    url_has_already_been_scraped = "No"
     forum_thread_page_num = 1
+    print("This url has not been scraped yet")
 
 
 """
@@ -59,9 +61,8 @@ starting_page = input("Enter number for which page of thread to start on. Leave 
 if starting_page != '':
     starting_page = int(starting_page)
     forum_thread_page_num = starting_page
-"""
 
-"""
+
 # User can enter which page of thread to stop collecting information on
 ending_page = input(
     "Enter number for which page of thread to end on. Leave blank to end on last page of thread. ").rstrip()
@@ -69,9 +70,8 @@ if ending_page == '':
     ending_page = float('inf')
 else:
     ending_page = int(ending_page)
-"""
 
-"""
+
 # create the csv with headers if csv does not already exist
 csv_file_path = pathlib.Path(file_name + '.csv')
 if csv_file_path.exists():
@@ -84,6 +84,8 @@ else:
 
 replies_info_df = pd.DataFrame()
 
+while_loop_iterator = 0
+
 while True:
     time.sleep(0.5)
     url = split_url + '/page-' + str(forum_thread_page_num)
@@ -95,29 +97,34 @@ while True:
     soup = bs.BeautifulSoup(response, 'lxml')
     #print(soup)
 
-    # Find the thread page-number navigation on the page. The last number on the navigation is the last page of the
+    # Find the thread page-number UI navigation on the page. The last number on the navigation is the last page of the
     # thread. if it cannot find the page-nav it means it does not exist and there is only one page for the thread
-    # If it does find that page-nav it will find the text (page numbers) and store the last one. Have to use [-2]
-    # because the last element is empty for some reason.
-    last_page_of_thread_soup = soup.find("ul", {"class": "pageNav-main"})
-    if last_page_of_thread_soup is None:
-        last_page_of_thread = 1
-        print("This thread has", last_page_of_thread, "pages.")
-    else:
-        last_page_of_thread = int((list(last_page_of_thread_soup)[-2].get_text()))
-        print("This thread has ", last_page_of_thread, " pages.")
+    # If it finds th¸ page-nav it will find the text (page numbers) and store the last one. Have to use [-2]
+    # because the last element is empty for some reason. Only need to get the last page number of thread once,
+    # so use while_loop_iterator to only get it once during the first while loop iteration.
+    if while_loop_iterator == 0:
+        last_page_of_thread_soup = soup.find("ul", {"class": "pageNav-main"})
+        if last_page_of_thread_soup is None:
+            last_page_of_thread = 1
+            print("This thread has", last_page_of_thread, "pages.")
+        else:
+            last_page_of_thread = int((list(last_page_of_thread_soup)[-2].get_text()))
+            print("This thread has ", last_page_of_thread, " pages.")
+    while_loop_iterator = 1
 
-    # get the thread title
-    thread_title_soup = soup.find("div", {"class": "p-title"})
-    thread_title = thread_title_soup.get_text()
-    thread_title = thread_title.replace('\n', ' ')
+    # Get the thread title only when scraping the first page. Don't need to rescrape the title for every page.
+    if forum_thread_page_num == 1:
+        thread_title_soup = soup.find("div", {"class": "p-title"})
+        thread_title = thread_title_soup.get_text()
+        thread_title = thread_title.replace('\n', ' ')
+        print("getting thread title")
 
     # create list of usernames so that they can later be matched to comments
     username_list = []
-    username = soup.findAll("a", {"itemprop": "name"})
-    for name in username:
-        new_name = name.get_text()
-        username_list.append(new_name)
+    username_soup = soup.findAll("a", {"itemprop": "name"})
+    for username in username_soup:
+        name = username.get_text()
+        username_list.append(name)
 
     # find the date & time that user's comments were posted
     date_soup = soup.findAll("ul", {'class': 'message-attribution-main listInline'})
@@ -130,7 +137,7 @@ while True:
         date_time_list.append(date_time)
 
     # find the thread's comments
-    comments = soup.findAll("div", {"class": "bbWrapper"})
+    comment_soup = soup.findAll("div", {"class": "bbWrapper"})
 
     # Replies that quote other users will contain ("div", {"class": "bbCodeBlock-expandContent"}). If this is found
     # 'Quote' is added to the quote_reply_list Else: No Quote is added to list. To access comment, return only the
@@ -138,7 +145,7 @@ while True:
     # of -> "div", {"class": "bbWrapper"}
     comment_list = []
     quote_reply_list = []
-    for comment in comments:
+    for comment in comment_soup:
         replies_to_quotes = comment.find_all("div", {"class": "bbCodeBlock-expandContent"})
         if len(replies_to_quotes) > 0:
             quoted_or_not = "quote"
@@ -180,26 +187,33 @@ while True:
     # the script will run this if statement when on the last page of the thread.
     if forum_thread_page_num == last_page_of_thread:
         # Rename the columns of the dataframe
-        replies_info_df = replies_info_df.rename(columns={0: "username", 1: "date_time", 2: "score", 3: "quoted", 4: "sentiment",
-                                        5: "replies"})
+        replies_info_df = replies_info_df.rename(columns={0: "username", 1: "date_time", 2: "score", 3: "quoted",
+                                                          4: "sentiment", 5: "replies"})
 
         # Convert date & time columns from string to datetime objects so that they can be manipulated with pandas
         replies_info_df["date_time"] = pd.to_datetime(replies_info_df["date_time"], format="%Y-%m-%d %H:%M:%S")
 
-        # print("\n--------------------\nTotal Replies: ", replies_info_df["username"].count(), "\n--------------------")
+        # If the this URL has already been scraped there is no need to add the thread info to the db again.
+        # If it has not already been scraped, convert add the first row of the replies_info_df to a dictionary and remove
+        # it from the dataframe. The first row of the dataframe is the creator of the post and the first post of the
+        # thread. This dictionary will be uploaded to the threads table.
+        if url_has_already_been_scraped == "No":
+            thread_info_dict = replies_info_df.to_dict('records')[0]
+            pop_list = ['sentiment', 'quoted', 'score']
+            for word in pop_list:
+                thread_info_dict.pop(word)
+            thread_info_dict.update({'url': split_url})
+            thread_info_dict.update({'title': thread_title})
+            thread_info_dict.update({'last_page_scraped': forum_thread_page_num})
+            replies_info_df = replies_info_df.drop(replies_info_df.index[0])
 
-        # convert the first row of the dataframe to a dictionary and remove it from the dataframe. The first row of the
-        # dataframe is the creator of the post and the first post of the thread. This dictionary will be uploaded to a
-        # different table.
-        first_comment_of_thread_dict = replies_info_df.to_dict('records')[0]
-        pop_list = ['sentiment', 'quoted', 'score']
-        for word in pop_list:
-            first_comment_of_thread_dict.pop(word)
-        first_comment_of_thread_dict.update({'url': split_url})
-        first_comment_of_thread_dict.update({'title': thread_title})
-        first_comment_of_thread_dict.update({'last_page_scraped': forum_thread_page_num})
-        replies_info_df = replies_info_df.drop(replies_info_df.index[0])
-
+            # insert url, thread title, etc.. into polls_threads table. 'OR IGNORE' will ignore if record exists
+            table = Table('polls_threads', metadata, autoload=True, autoload_with=engine)
+            insert_statement = insert(table).values(thread_info_dict)
+            results = connection.execute(insert_statement)
+            print("new thread title added to DB!")
+        else:
+            print("Thread title not added. It has already been scraped!")
 
         """
         # ------Total Replies on a date in Chron order-------#
@@ -315,12 +329,7 @@ while True:
         replies_info_df.to_csv(csv_file_path, index=False, mode="a", header=False, encoding='utf-8-sig')
         """
 
-        # insert url, thread title, etc.. into polls_threads table. 'OR IGNORE' will ignore if record exists
-        table = Table('polls_threads', metadata, autoload=True, autoload_with=engine)
-        insert_statement = insert(table).values(first_comment_of_thread_dict).prefix_with("OR IGNORE")
-        results = connection.execute(insert_statement)
-
-        # SELECT the primary key 'ID' from the polls_threads record that was just inserted to use as foreign key in
+        # SELECT the primary key 'ID' from the polls_threads record for the URL to use as foreign key in
         # polls_posts
         url_id_query = connection.execute("SELECT id FROM polls_threads WHERE url = ?", split_url)
         id_num = url_id_query.fetchone()[0]
@@ -329,10 +338,11 @@ while True:
         # will serve as a foreign key to the polls_threads table
         replies_info_df.insert(0, 'thread_id', id_num)
 
-        # insert username, replies ,etc..  into polls_posts table
-        insert_values_forum_table = replies_info_df.to_dict(orient='records')
+        # insert usernames, replies, reply sentiment, time of reply dict. into polls_posts table. 'OR IGNORE' will
+        # will skip records that already exist.
+        replies_info_dict = replies_info_df.to_dict(orient='records')
         table = Table('polls_posts', metadata, autoload=True, autoload_with=engine)
-        insert_statement = insert(table).values(insert_values_forum_table).prefix_with("OR IGNORE")
+        insert_statement = insert(table).values(replies_info_dict).prefix_with("OR IGNORE")
         results = connection.execute(insert_statement)
 
         # close the db connection to prevent a database error
